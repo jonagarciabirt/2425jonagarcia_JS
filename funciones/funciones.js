8c9db626603386f5da0a145ec5de3cecc3843f95
jonagarciabirt <122236080+jonagarciabirt@users.noreply.github.com>
2024-11-15 09:55:49 +0100

    .

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -1,1 +1,25 @@
-/*
+// Cuando el usuario hace scroll, se ejecuta la función scrollFunction
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    var BotonArriba = document.getElementById("BotonArriba");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      BotonArriba.style.display = "block";
+    } else {
+      BotonArriba.style.display = "none";
+    }
+  }
+  
+  // Al hacer clic en el botón, se vuelve al inicio de la página
+  document.addEventListener("DOMContentLoaded", function() {
+    var BotonArriba = document.getElementById("BotonArriba");
+    BotonArriba.addEventListener("click", function() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
+  
